62835d3bcd364b06300f82d74f26c33a784df38b
carmentenorio <carmen.tenorio.btsoft@gmail.com>
2025-10-06 15:42:18 -0400

    apply a template (header, footer, and main content) to begin working with the project design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css';
-import { Route,Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/Navbar';
+import Footer from './components/Footer';
+import Task from './pages/Task';
+import Category from './pages/Category';
+import Tag from './pages/Tag';
 
 function App() {
- return (
-    <Routes>
-      <Route path="/" element={<h1>Hello World</h1>} />
-    </Routes>
+  return (
+    <div className="d-flex flex-column min-vh-100 w-100">
+      <NavigationBar />
+      <main className="flex-fill w-100">
+        <Routes>
+          <Route path="/task" element={<Task />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/tag" element={<Tag />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/pages/Task.jsx
@@ -0,0 +1,8 @@
+function Task() {
+  return(
+  <div className="w-100 h-100 d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+    <h1>Hello World</h1>
+  </div>
+  );
+}
+export default Task;
