361738c2f784297eb8110b931bbc95241dd4105f
carmentenorio <carmen.tenorio.btsoft@gmail.com>
2025-10-07 16:52:29 -0400

    PR-8 : Create /task route and display task list from TaskService (no actions)

---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import TaskService from '../services/TaskService';
+
+function Task() {
+  const [tasks, setTasks] = useState([]);
+
+  const taskAll = async () => {
+    try {
+      const data = await TaskService.getAll();
+      console.log("tasks:", data);
+      setTasks(data.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    taskAll();
+  }, []);
+
+  return (
+    <div className="container mt-5">
+      <h2 className="mb-4 text-center">List of Tasks</h2>
+
+      <div className="table-responsive shadow-sm rounded">
+        <table className="table table-striped table-hover align-middle">
+          <thead className="table-dark">
+            <tr>
+              <th></th>
+              <th scope="col">Title</th>
+              <th scope="col">Description</th>
+              <th scope="col">State</th>
+              <th scope="col">Accions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.length > 0 ? (
+              tasks.map((task, index) => (
+                <tr key={task.id}>
+                  <td>{index + 1}</td>
+                  <td>{task.title}</td>
+                  <td>{task.description ?? "Sin descripción"}</td>
+                  <td>
+                    {task.completed ? (
+                      <span className="badge bg-success">Completed</span>
+                    ) : (
+                      <span className="badge bg-warning text-dark">Earring</span>
+                    )}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="4" className="text-center py-3">
+                  There are no tasks available.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Task;
