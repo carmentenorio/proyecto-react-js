7a9ae9e209e9e2218b857a1d4946caa4c4d9ed44
carmentenorio <carmen.tenorio.btsoft@gmail.com>
2025-10-13 10:19:23 -0400

    Implement /category/:id route to display a specific category using CategoryService.getOne()  (#6)

---
+++ src/pages/CategoryViewPage.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import CategoryService from '../services/CategoryService';
+import { useParams } from 'react-router-dom';
+
+function CategoryView() {
+    const { id } = useParams();
+    const [category, setCategory] = useState(null);
+
+    useEffect(() => {
+        const fetchCategory = async () => {
+            try {
+                const response = await CategoryService.getOne(id);
+                setCategory(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchCategory();
+    }, [id]);
+
+    if (!category)
+        return <p className="text-center mt-5">Category not found</p>;
+
+    return (
+        <div className="container mt-5">
+            <h2 className="mb-4 text-center">Category details</h2>
+            <div className="card shadow-sm p-4">
+                <p><strong>ID:</strong> {category.id}</p>
+                <p><strong>Name:</strong> {category.name}</p>
+            </div>
+        </div>
+    );
+}
+
+export default CategoryView;
