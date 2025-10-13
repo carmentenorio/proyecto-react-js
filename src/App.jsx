772e2db48138e9ef497ae88c7aadd3177e14dc31
carmentenorio <carmen.tenorio.btsoft@gmail.com>
2025-10-13 18:51:02 -0400

    PR-12: adding code to file delete category

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/Navbar';
 import Footer from './components/Footer';
 import Task from './pages/TaskPage';
-import Category from './pages/Category';
-import CategoryView from './pages/CategoryView';
+import Category from './pages/CategoryListPage';
+import CategoryView from './pages/CategoryViewPage';
 import CategoryCreate from './pages/CategoryCreatePage';
 import CategoryUpdate from './pages/CategoryUpdate';
 
